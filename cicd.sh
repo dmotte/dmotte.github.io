f95b641c8bcbae5d46359bb17e1022ea69bd2d6e
dmotte <37443982+dmotte@users.noreply.github.com>
2025-01-04 15:02:44 +0100

    Added incomplete cicd.sh

---
+++ cicd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Ensure that some variables are defined
+: "${MISC_SCRIPTS_DIR:?}"
+
+cd "$(dirname "$0")"
+
+readonly username=${1:?} out_txt=${2:?} out_html=${3:?} description=${4:-}
+
+data=$(bash "$MISC_SCRIPTS_DIR/github-get-all-repos.sh" "users/$username" \
+    '.archived == false and .fork == false' \
+    '.name, .description, .homepage, .topics')
+echo "$data" | tr -d '\r' > "$out_txt"
+
+# TODO
+
+[ -z "$(git status -s)" ] || {
+    echo 'There are some uncommitted changes' >&2
+    git diff
+    exit 1
+}

b5a92db137a01d788615981013781a0da61131ef
dmotte <37443982+dmotte@users.noreply.github.com>
2025-01-05 16:09:59 +0100

    Aligned Bash with HTML

--- cicd.sh
+++ cicd.sh
@@ -23,8 +23,44 @@ echo "$data" | tr -d '\r' > "$out_txt"
 # TODO link to GH profile page
 
 tee -a "$out_html" << EOF
-<h1>$username</h1>
-TODO first part
+<!doctype html>
+<html>
+  <head>
+    <title>$username's projects</title>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="favicon.svg" />
+
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    />
+
+    <style type="text/css">
+      /* Generated using the Customizer on https://bulma.io/ */
+      :root {
+        --bulma-primary-h: 215deg;
+        --bulma-primary-s: 100%;
+        --bulma-primary-l: 50%;
+        --bulma-link-h: 215deg;
+        --bulma-link-s: 100%;
+        --bulma-link-l: 50%;
+      }
+    </style>
+  </head>
+
+  <body>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">$username</h1>
+
+        <div class="block">
+          This is the list of all my <strong>projects</strong>.
+        </div>
+
+        <div class="grid is-col-min-12">
 EOF
 
 while read -r name; do
@@ -32,13 +68,34 @@ while read -r name; do
     read -r homepage
     read -r _topics
 
-    echo "TODO name: $name"
-    echo "TODO description: $description"
-    echo "TODO homepage: $homepage"
+    echo '          <div class="cell">'
+    echo '            <div class="card">'
+    echo '              <div class="card-content">'
+    echo '                <div class="content">'
+    echo '                  <p class="title is-4">'
+    echo '                    &#x1F30D; docker-portmap-server-rootless'
+    echo '                  </p>'
+    echo '                  <p>'
+    echo '                    Docker image that can be used for remote port forwarding'
+    echo '                    (rootless version)'
+    echo '                  </p>'
+    echo '                </div>'
+    echo '              </div>'
+    echo '              <footer class="card-footer">'
+    echo '                <a href="#" class="card-footer-item">&#x1F30D; Homepage</a>'
+    echo '                <a href="#" class="card-footer-item">&#x1F4C1; Repo</a>'
+    echo '              </footer>'
+    echo '            </div>'
+    echo '          </div>'
+    break # TODO
 done < "$out_txt" | tee -a "$out_html"
 
 tee -a "$out_html" << EOF
-TODO last part
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
 EOF
 
 ################################################################################

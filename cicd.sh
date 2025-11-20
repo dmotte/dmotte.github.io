65c801ade1fd2fad9e82ddf6560c4466d643eb36
dmotte <37443982+dmotte@users.noreply.github.com>
2025-11-20 23:40:14 +0100

    Added IFS= to read where needed

--- cicd.sh
+++ cicd.sh
@@ -97,10 +97,10 @@ cat << EOF >> "$out_html"
         <div class="grid is-col-min-14 is-gap-3">
 EOF
 
-while read -r name; do
-    read -r description
-    read -r homepage
-    read -r topics
+while IFS= read -r name; do
+    IFS= read -r description
+    IFS= read -r homepage
+    IFS= read -r topics
 
     emoji=$(echo "$description" | cut -d' ' -f1)
     desc_without_emoji=$(echo "$description" | cut -d' ' -f2-)
